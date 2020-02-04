ba0e21ef951aef981eb616289bab8ee5dfc270bc
Baptiste TOULEMONDE <baptistetoul@gmail.com>
2020-02-04 19:38:39 +0100

    test

--- src/main/java/dev/collegues/Startup.java
+++ src/main/java/dev/collegues/Startup.java
@@ -34,7 +34,7 @@ public class Startup {
 
 		LOG.info("Démarrage de l'application");
 
-		if (this.collegueRepository.count() == 0) {
+		if (this.collegueRepository.count() == 3) {
 
 			List<Collegue> collegues = new ArrayList<Collegue>();
 			collegues.add(new Collegue(UUID.randomUUID().toString(), "Picasso", "Pablo", "pablo.picasso@gmail.com",
@@ -42,6 +42,8 @@ public class Startup {
 			collegues.add(new Collegue(UUID.randomUUID().toString(), "Hugo", "Victor", "victor.hugo@gmail.com",
 					LocalDate.of(1900, 10, 05), "http://www.w3schools.com/bootstrap/img_avatar3.png"));
 			collegues.add(new Collegue(UUID.randomUUID().toString(), "Chan", "Jacky", "chan.jacky@gmail.com",
+					LocalDate.of(1900, 10, 05), "http://www.w3schools.com/bootstrap/img_avatar3.png"));
+			collegues.add(new Collegue(UUID.randomUUID().toString(), "Picasso", "Paul", "paul.picasso@gmail.com",
 					LocalDate.of(1900, 10, 05), "http://www.w3schools.com/bootstrap/img_avatar3.png"));
 			this.collegueRepository.saveAll(collegues);
 
